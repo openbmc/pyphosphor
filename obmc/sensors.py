65d747c3ad2f5ee037d9ddb16d25fcc3866a3c16
Yi Li <adamliyi@msn.com>
2016-07-01 16:47:16 +0800

    pyphosphor: Add parameter to Emergency signal of SenorThresholds
    When sensor value is out of range of threshold, an "Emergency"
    signal will be emitted. Add a 'message' parameter to the signal,
    to provide detail information on the cause of 'Emergency'.
    This information will be logged in an ESEL.
    
    This patch is part of the fix for issue:
    https://github.com/openbmc/skeleton/issues/64
    
    Change-Id: I501188b36718b8a87582558c349c0316e1092db5
    Signed-off-by: Yi Li <adamliyi@msn.com>

--- obmc/sensors.py
+++ obmc/sensors.py
@@ -111,7 +111,9 @@ class SensorThresholds(DbusProperties):
         if self.Get(iface, 'threshold_state') != current_state and \
                 current_state == "CRITICAL" and \
                 self.Get(iface, 'emergency_enabled') is True:
-            self.Emergency()
+            message = IFACE_NAME + ': ' + type(self).__name__ + \
+			' :check_thresholds triggered emergency: ' + str(value)
+            self.Emergency(message)
 
         self.Set(iface, 'threshold_state', current_state)
         worst = self.properties[iface]['worst_threshold_state']
@@ -121,8 +123,8 @@ class SensorThresholds(DbusProperties):
 
         return rtn
 
-    @dbus.service.signal(IFACE_NAME, signature='')
-    def Emergency(self):
+    @dbus.service.signal(IFACE_NAME, signature='s')
+    def Emergency(self, message):
         pass
 
 
